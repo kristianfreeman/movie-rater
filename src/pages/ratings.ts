bdcc1e3fb9d3e8ac186cc234ba4980a2969eba25
Kristian Freeman <kristian@kristianfreeman.com>
2024-11-18 13:46:35 -0600

    Add simple example

--- src/pages/ratings.ts
+++ src/pages/ratings.ts
@@ -2,18 +2,28 @@ import type { APIRoute } from "astro";
 
 type Rating = {
   movie_id: number;
-  user_id: string;
   rating: number;
 }
 
-export const GET: APIRoute = async ({ locals }) => {
-  const { DB } = locals.runtime.env;
-  const ratings = await DB.prepare("SELECT * FROM ratings").all<Rating>();
-  const asObject = ratings.results.reduce((acc, rating) => {
-    acc[rating.movie_id] = rating;
-    return acc;
-  }, {} as Record<number, Rating>);
-  return Response.json(asObject);
+export const GET: APIRoute = async () => {
+  // const { DB } = locals.runtime.env;
+  // const ratings = await DB.prepare("SELECT * FROM ratings").all<Rating>();
+  //const asObject = ratings.results.reduce((acc, rating) => {
+  //  acc[rating.movie_id] = rating;
+  //  return acc;
+  //}, {} as Record<number, Rating>);
+
+  const ratings: Record<number, Rating> = {
+    1: {
+      movie_id: 1,
+      rating: 1
+    }, 2: {
+      movie_id: 2,
+      rating: 2
+    }
+  }
+
+  return Response.json(ratings);
 }
 
 type RatingRequest = {
@@ -21,9 +31,9 @@ type RatingRequest = {
   rating: number;
 }
 
-export const POST: APIRoute = async ({ locals, request }) => {
-  const { DB } = locals.runtime.env;
+export const POST: APIRoute = async ({ request }) => {
   const { movieId, rating } = await request.json<RatingRequest>();
-  await DB.prepare("INSERT INTO ratings (movie_id, rating) VALUES (?, ?)").bind(movieId, rating).run();
-  return Response.json("OK");
+  //const { DB } = locals.runtime.env;
+  //await DB.prepare("INSERT INTO ratings (movie_id, rating) VALUES (?, ?)").bind(movieId, rating).run();
+  return Response.json({ movieId, rating });
 }
